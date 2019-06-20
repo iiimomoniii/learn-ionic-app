11ea7d524346e0446d5fbc74c75f5ee5455e5f73
iiimomoniii <natdanai_thaiyala@live.com>
2019-06-20 15:03:00 +0700

    change

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipes.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/2560px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Spaghettata.JPG',
+      ingredients: ['Spaghetti', 'Meat', 'Tomato']
+    }
+  ];
+  constructor() { }
+
+  getAllRecipes(){
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: string){
+    return {...this.recipes.find(recipe => {
+      return recipe.id === recipeId;
+    })};
+  }
+
+}
